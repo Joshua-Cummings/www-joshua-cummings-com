70da07733044737958d48b5509b54ef6a67cfe3d
Joshua Cummings <josh@joshua-cummings.com>
2026-04-28 11:58:02 -0400

    feat(home): add nav links

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+interface NavLink {
+  path: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly name = 'Joshua Cummings'
   protected readonly title = signal('Joshua Cummings | IT Professional');
+  navLinks = signal<NavLink[]>([
+    { path: '/projects', label: 'View Projects' },
+    { path: '/hire', label: 'Hire Me' },
+    { path: '/resume', label: 'Resume' },
+    { path: '/contact', label: 'Contact' }
+  ]);
 }
